0a18c8bae6d506bd5065defba69a47dad8a1936d
RodrigoEsprella <hairaxelpoderoso@gmail.com>
2024-09-25 22:19:54 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center bg-gray-200'>
+      <h1 className='text-4xl text-blue-500'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;

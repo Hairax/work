58393b0a300942bae41edbdd8d020f3607e49bdf
Rodrigo Esprella <hairaxelpoderoso@gmail.com>
2024-09-27 10:17:58 -0400

    create a header responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './componentes/Header/Header';
 function App() {
   return (
-    <div className='h-screen flex justify-center items-center bg-gray-200'>
-      <h1 className='text-4xl text-blue-500'>Hello World</h1>
+    <div className=' items-center bg-gray-200'>
+      <Header />
     </div>
   );
 }

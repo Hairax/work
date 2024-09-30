2979f11816061c1137741907ec7135312fd328c2
Rodrigo Esprella <hairaxelpoderoso@gmail.com>
2024-09-30 16:47:11 -0400

    feat: add tooltip header

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -14,20 +14,24 @@ function Header() {
         <nav className="flex justify-center py-4">
             <div className="inline-flex items-center space-x-6 dark:bg-gray-800 bg-white rounded-full px-4 py-4">
                 <ul className="flex space-x-6">
-                    <li>
+                    <li className="relative group">
                         <a href="/" className={`nav-icon text-gray-800 dark:text-white flex items-center ${getIconClass("/")}`}>
                             <MdHome className="w-10 h-10" />
                         </a>
+                        {/* Tooltip */}
+                        <span className="tooltip-text">Home</span>
                     </li>
-                    <li>
+                    <li className="relative group">
                         <a href="/about" className={`nav-icon text-gray-700 dark:text-white flex items-center ${getIconClass("/about")}`}>
                             <MdBookmark className="w-10 h-10" />
                         </a>
+                        <span className="tooltip-text">About</span>
                     </li>
-                    <li>
+                    <li className="relative group">
                         <a href="/services" className={`nav-icon text-gray-800 dark:text-white flex items-center ${getIconClass("/services")}`}>
                             <MdPlayArrow className="w-10 h-10" />
                         </a>
+                        <span className="tooltip-text">Services</span>
                     </li>
                 </ul>
             </div>

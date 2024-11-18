bdab82478f215ae4e1fcddb72047c631ae716658
Rodrigo Esprella <hairaxelpoderoso@gmail.com>
2024-11-18 18:39:58 -0400

    desktop electron

--- src/Layout.js
+++ src/Layout.js
@@ -4,12 +4,18 @@ import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
+<<<<<<< HEAD
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar />
 
       {/* Contenido de la página */}
       <div className="flex-1 ml-20 p-8 overflow-y-auto">
+=======
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 ml-20 p-8">
+>>>>>>> b6de3ae (desktop electron)
         <Outlet />
       </div>
     </div>
